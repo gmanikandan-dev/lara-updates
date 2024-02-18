6ab178620077d6d709a120724dcca2c3e811b787
Manikandan G <manikandan.g@targetbay.com>
2024-02-18 12:45:48 +0530

    http pool fater by 50%

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Http\Client\Pool;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,30 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/http-pool', function () {
+    $startHttpGet = now();
+    $urls = [
+        'https://reqres.in/api/users/2',
+        'https://reqres.in/api/users/3',
+        'https://reqres.in/api/users/4',
+    ];
+    foreach ($urls as $url) {
+        $httpGetResponse = Http::get($url);
+        dump($httpGetResponse->json());
+    }
+
+    dump(now()->diffInMilliseconds($startHttpGet));
+
+    $startHttpPool = now();
+    $httpPoolResponses = Http::pool(fn (Pool $pool) => [
+        $pool->get('https://reqres.in/api/users/2'),
+        $pool->get('https://reqres.in/api/users/3'),
+        $pool->get('https://reqres.in/api/users/4'),
+    ]);
+
+    foreach ($httpPoolResponses as $response) {
+        dump($response->json());
+    }
+    dump(now()->diffInMilliseconds($startHttpPool), $httpPoolResponses);
 });
